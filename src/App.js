d94d696ee1329f33f1566876a97cce09bfb33586
aSenPer <zjt18369656127@gmail.com>
2021-08-21 11:16:50 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.less';
+import { message } from 'antd';
+import { inject, observer } from 'mobx-react';
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+import { main as mainConfig } from './router/index';
+import { RenderRoutes } from './router/allocation';
+import { getUserInfo } from './api/login';
+@inject('store')
+@observer
+class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {};
+	}
+	checkPermisson = id => {
+		getUserInfo(id).then(res => {
+			console.log(res);
+			if (!res || !res.user) {
+				message.error({
+					content: '登录状态已过期，请重新登录',
+					duration: 2,
+					onClose: () => {
+						this.props.store.logout();
+						this.props.history.push('/');
+						// window.location.reload();
+					},
+				});
+			} else {
+				// this.props.history.push('/index');
+			}
+		});
+	};
+	componentDidMount() {
+		console.log(this.props);
+		const { id, ticket } = this.props.store;
+		if (id && ticket) {
+			// this.checkPermisson(id);
+		}
+	}
+	render() {
+		const { authed } = this.props.store;
+		const { pathname } = this.props.location;
+		return (
+			<div className="App">
+				{/* <Redirect to="/login"></Redirect> */}
+				<RenderRoutes
+					routes={mainConfig}
+					authed={authed}
+					authPath="/login"
+					route={pathname}
+				></RenderRoutes>
+			</div>
+		);
+	}
 }
 
-export default App;
+export default withRouter(App);
--- src/index.js
+++ src/index.js
@@ -1,14 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'mobx-react';
+import { HashRouter } from 'react-router-dom';
+import { ErrorBoundary } from 'react-error-boundary';
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import Store from './store/index';
+function ErrorFallback({ error, resetErrorBoundary }) {
+	return (
+		<div role="alert">
+			<p>Something went wrong:</p>
+			<pre>{error.message}</pre>
+			<button onClick={resetErrorBoundary}>Try again</button>
+		</div>
+	);
+}
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	// <ErrorBoundary
+	// 	FallbackComponent={ErrorFallback}
+	// 	onReset={() => {
+	// 		// reset the state of your app so the error doesn't happen again
+	// 	}}
+	// >
+	<Provider store={Store}>
+		<HashRouter>
+			<App />
+		</HashRouter>
+	</Provider>,
+	// </ErrorBoundary>,
+	document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
